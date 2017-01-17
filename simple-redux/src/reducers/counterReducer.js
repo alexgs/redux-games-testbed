7f1f6df4a421aec4ea6e3e2a7139ce43fd0208ab
Phil Gates-Shannon <phil.gs@gmail.com>
2017-01-17 06:42:42 -0500

    [WIP] Refactor app component to work with immutable store

--- simple-redux/src/reducers/counterReducer.js
+++ simple-redux/src/reducers/counterReducer.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import Immutable from 'immutable';
+// import { ActionTypes } from 'redux/src/createStore';
 import { INCREMENT_COUNTER } from '../constants';
 
 let initialState = Immutable.Map( { value: 0 } );
@@ -9,6 +10,7 @@ const counter = function counterReducer( state = initialState, action ) {
     if ( !Immutable.Map.isMap( state ) ) {
         throw new Error( 'Argument `state` must be type Immutable.Map' );
     }
+    // if ( !( Immutable.Map.isMap( action ) || action.type === ActionTypes.INIT ) ) {
     if ( !Immutable.Map.isMap( action ) ) {
         throw new Error( 'Argument `action` must be type Immutable.Map' );
     }
