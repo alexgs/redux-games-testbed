1c213eda47b926dee4c4fdea2b22304977b92a36
Phil Gates-Shannon <phil.gs@gmail.com>
2017-01-17 05:48:44 -0500

    Refactor "simple store" to use immutable state

--- simple-redux/src/store/simpleStore.js
+++ simple-redux/src/store/simpleStore.js
@@ -1,11 +1,18 @@
 'use strict';
 
+import Immutable from 'immutable';
+
 import counterReducer from '../reducers/counterReducer';
 import simpleActionCreator from '../actions/simpleActionCreators';
 
 function createStore( reducer, initialState ) {
-    let state = initialState;
-    let subscribers = [ ];
+    let state = null;
+    if ( Immutable.Map.isMap( initialState ) ) {
+        state = initialState;
+    } else {
+        throw new Error( 'Argument `initialState` must be type Immutable.Map' );
+    }
+    let subscribers = Immutable.List();
 
     function dispatch( action ) {
         state = reducer( state, action );
@@ -17,13 +24,13 @@ function createStore( reducer, initialState ) {
     }
 
     function subscribe( callback ) {
-        subscribers.push( callback );
+        subscribers = subscribers.push( callback );
     }
 
     return { dispatch, getState, subscribe };
 }
 
-let startingValue = 0;
+let startingValue = Immutable.Map( { value: 0 } );
 let incrementAction = simpleActionCreator.increment();
 let store = createStore( counterReducer, startingValue );
 
